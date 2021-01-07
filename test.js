01ed080fb30ca38064b41ad4102280a51be398e6
Mike Roylance <roylance.michael@gmail.com>
2021-01-06 19:10:44 -0700

    working - minus "msg" or "channel" send

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+// https://www.npmjs.com/package/jsdom
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+// https://www.npmjs.com/package/discord.js?source=post_page-----7b5fe27cb6fa----------------------
+const Discord = require('discord.js');
+
+// https://discordjs.guide/popular-topics/embeds.html#using-the-richembedmessageembed-constructor
 const myregexp = /\/assets\/img\/raid-bosses\/graphic(.*?)\jpg/
 
 JSDOM.fromURL("https://leekduck.com/boss/", {}).then(dom => {
@@ -10,7 +15,9 @@ JSDOM.fromURL("https://leekduck.com/boss/", {}).then(dom => {
     console.log(raidbossURL);
 
     // discord specific api?
-    // exampleEmbed = new discord.MessageEmbed().setTitle('Current Pokemon Go Raid Bosses!').setURL(raidbossURL)
+    const exampleEmbed = new Discord.MessageEmbed().setTitle('Current Pokemon Go Raid Bosses!').setURL(raidbossURL);
+    console.log(exampleEmbed);
+    // is this running in a bot? respond to msg, or channel
     // msg.channel.send({ embed: exampleEmbed })
   }
 });
